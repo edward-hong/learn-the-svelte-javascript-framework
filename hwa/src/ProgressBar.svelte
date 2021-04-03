25e253a883f4317397679c55e05f24dbc695949b
Edward Hong <edward.hong527@gmail.com>
2021-04-03 19:52:46 +1300

    Components & HTML

---
+++ hwa/src/ProgressBar.svelte
@@ -0,0 +1,19 @@
+<div bp="grid">
+  <div class="progress-container" bp="offset-5@md 4@md 12@sm">
+    <div class="progress-bar" style="width: 20%;">
+      <span class="sr-only">%</span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .progress-container {
+    margin: 20px 0;
+    border: 1px solid rgb(194, 194, 194);
+  }
+
+  .progress-bar {
+    height: 30px;
+    background-color: rgb(77, 199, 55);
+  }
+</style>
